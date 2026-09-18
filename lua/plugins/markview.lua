8739e514e95dc4eedc5737bb80ae8b59436dbde4
Jeremy Lumley <jeremy.lumley96@gmail.com>
2026-09-18 09:55:39 -0400

    markview toogle checkbox

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -7,4 +7,13 @@ return {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
   },
+
+  keys = {
+    { '<leader>x', '<cmd>Checkbox toggle<CR>', ft = 'markdown', mode = { 'n', 'v' }, desc = 'Toggle markdown checkbox' },
+  },
+
+  config = function()
+    require('markview').setup()
+    require('markview.extras.checkboxes').setup()
+  end,
 }
